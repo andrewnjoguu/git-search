d2fdb28308ee56a3f13f60574e38b462da11f2e5
andrewnjoguu <andrew.njogu@student.moringaschool.com>
2021-10-20 12:19:23 +0300

    api image inserted for better understanding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitDetailsComponent } from './git-details/git-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GitPipe } from './git.pipe';
+import { GitDirectDirective } from './git-direct.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitDetailsComponent
+    GitDetailsComponent,
+    GitPipe,
+    GitDirectDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/git-direct.directive.spec.ts
@@ -0,0 +1,8 @@
+import { GitDirectDirective } from './git-direct.directive';
+
+describe('GitDirectDirective', () => {
+  it('should create an instance', () => {
+    const directive = new GitDirectDirective();
+    expect(directive).toBeTruthy();
+  });
+});
